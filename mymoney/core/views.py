d853bfa9f025a85da7db5dbf1fc1fb3844c98503
Yannick Chabbert <yannick.chabbert@gmail.com>
2015-11-30 13:46:57 +0100

    remove django settings

--- mymoney/core/views.py
+++ mymoney/core/views.py
@@ -1,10 +1,6 @@
-import json
-
 from django.conf import settings
 from django.utils.translation import get_language, to_locale
 from django.views.generic import TemplateView
-
-from rest_framework.settings import api_settings
 
 
 class HomeView(TemplateView):
@@ -14,17 +10,8 @@ class HomeView(TemplateView):
         context = super(HomeView, self).get_context_data()
         locale = to_locale(get_language())
 
-        context['django_settings'] = json.dumps({
-            'static_url': settings.STATIC_URL,
-            'locale': locale,
-            'xsrf_cookiename': settings.CSRF_COOKIE_NAME,
-            'non_field_errors_key': api_settings.NON_FIELD_ERRORS_KEY,
-            'debug': settings.DEBUG,
-        })
-
         if settings.MYMONEY['USE_L10N_DIST']:
             context['dist_l10n_src'] = \
-                "mymoney/dist/js/locales/{locale}.min.js".format(
-                    locale=locale)
+                "mymoney/dist/js/locales/{locale}.min.js".format(locale=locale)
 
         return context
